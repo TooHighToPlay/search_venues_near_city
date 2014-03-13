a282d6ebe96bc60664ef720f64dbfd6017653298
Zilvinas Kucinskas <KevinGee@zilvinass-mbp.zyxel.com>
2014-03-13 22:33:14 +0100

    Upload project to git

---
+++ src/iwa3.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, request, jsonify
+from crossdomain import crossdomain
+
+from SPARQLWrapper import SPARQLWrapper, RDF, JSON
+import requests
+import json
+
+__author__ = 'KevinGee'
+
+app = Flask(__name__)
+
+CLIENT_ID = 'HFF2H1AU5GGMISF3AFJFQNVWV40RZQGRFSA2U3F5SFZLK5DR'
+CLIENT_SECRET = 'F0CYPMQPI0ANCT13MORDWPJABFGJPLJQ1CC1JLKGVHMSLCZQ'
+VERSION = '20140101'
+
+@app.route('/')
+def first_page():
+    app.logger.debug('You arrived at ' + url_for('first_page'))
+    return render_template('index.html')
+
+@app.route('/search_venues', methods=['GET'])
+def search_venues():
+    app.logger.debug('You arrived at ' + url_for('search_venues'))
+
+    query = request.args.get('query', '')
+    app.logger.debug('Query is ' + query)
+    near = request.args.get('near', '')
+    app.logger.debug('Location is ' + near)
+
+    url = 'https://api.foursquare.com/v2/venues/search?client_id=%s'\
+            '&client_secret=%s&v=%s&query=%s&near=%s' %\
+            (CLIENT_ID, CLIENT_SECRET, VERSION, query, near)
+    r = requests.get(url)
+    if r.ok:
+        venues = r.json()['response']['venues']
+        venues = [v for v in venues if 'url' in v]
+    else:
+        venues = []
+
+    return jsonify(data=venues)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ src/static/js/iwa3.js
@@ -0,0 +1,38 @@
+var map = null;
+
+var amsterdam = new google.maps.LatLng(52.37022, 4.89517);
+
+$( document ).ready(function() {
+  	var bootstrapCSSLink = $('<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">');
+	var bootstrapThemeCSSLink = $('<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">');
+	var bootstrapJavaScriptLink = $('<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>');
+
+	$('body').append(bootstrapCSSLink);
+	$('body').append(bootstrapThemeCSSLink);
+	$('body').append(bootstrapJavaScriptLink);
+});
+
+$('#submit').click( function() {
+    $('#map-canvas').hide();
+    $('#loading').append('<img src="http://i.imgur.com/KUJoe.gif">');
+
+    var venue = $('#venue').val();
+    var location = $('#location').val();
+
+
+
+    //$('#loading').empty();
+    //$('#map-canvas').show();
+});
+
+function initialize() {
+    var mapOptions = {
+        center: amsterdam,
+        zoom: 13
+    };
+
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
